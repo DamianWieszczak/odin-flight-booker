a1e9a7ebe5d067a0992d83e979575ca8cac4e9e1
DamianWieszczak <damian.wieszczak@gmail.com>
2022-10-04 15:39:46 +0200

    Update flight model, add columns to passenger model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,8 @@
 class Flight < ApplicationRecord
   validates :origin_airport_id, :destination_airport_id, :start_datetime, :flight_duration, presence: true
 
-  belongs_to :departure_airport, class_name: "Airport", foreign_key: "origin_airport_id"
-  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "destination_airport_id"
+  belongs_to :departure_airport, class_name: "Airport", foreign_key: "origin_airport_id", inverse_of: :arriving_flights
+  belongs_to :arrival_airport, class_name: "Airport", foreign_key: "destination_airport_id", inverse_of: :departing_flights
+
+  has_many :bookings
 end
