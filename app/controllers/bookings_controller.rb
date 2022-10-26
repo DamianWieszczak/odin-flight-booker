9fb8e59824c0fc6195a241e8a699adec67db90aa
DamianWieszczak <damian.wieszczak@gmail.com>
2022-10-26 21:58:23 +0200

    Set up mailer for passenger bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,6 +9,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
 
     if @booking.save
+      PassengerMailer.with(booking: @booking).confirmation_email.deliver_later
       redirect_to booking_path(@booking)
     else
       render "new"
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  default from: "odinflightbooker@example.com"
+
+  def confirmation_email
+    @booking = params[:booking]
+    mail(to: @booking.passengers.pluck(:email), subject: 'Your booking has been confirmed')
+  end
+
+
 end
